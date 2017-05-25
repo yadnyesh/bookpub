7861da63d601e24039595cd4bfc51a253dc5173b
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-05-25 22:23:21 +0530

    Properties

--- src/main/java/org/test/bookpub/org/test/bookpub/entity/Book.java
+++ src/main/java/org/test/bookpub/org/test/bookpub/entity/Book.java
@@ -1,8 +1,34 @@
 package org.test.bookpub.org.test.bookpub.entity;
 
+import javax.persistence.*;
+
 /**
  * Created by z063407 on 5/25/17.
  */
+
+@Entity
 public class Book {
+
+        @Id
+        @GeneratedValue
+        private Long id;
+        private String isbn;
+        private String title;
+        private String description;
+
+        @ManyToOne
+        private Author author;
+
+        @ManyToOne
+        private Publisher publisher;
+
+        @ManyToMany
+        private List<Reviewers> reviewers;
+
+        protected Book() {}
+
+
+
+
 
 }
