943a4b3c0eeaeea5e6f833f0b2abd12284fc091b
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-05-25 14:47:19 +0530

    First Commit

---
+++ src/main/java/org/test/bookpub/BookpubApplication.java
@@ -0,0 +1,18 @@
+package org.test.bookpub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BookpubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookpubApplication.class, args);
+	}
+
+	@Bean
+	public StartupRunner schedulerRunner() {
+		return new StartupRunner();
+	}
+}
---
+++ src/main/java/org/test/bookpub/StartupRunner.java
@@ -0,0 +1,19 @@
+package org.test.bookpub;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import javax.sql.DataSource;
+
+
+public class StartupRunner implements CommandLineRunner {
+    protected final Log logger = LogFactory.getLog(getClass());
+    @Autowired
+    private DataSource ds;
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("DataSource: "+ds.toString());
+    }
+}
