788976d0d7b4d621b3a756f9fce020959dbb605a
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-26 10:20:14 +0530

    Yes

--- src/main/java/org/test/bookpub/org/test/bookpub/entity/Author.java
+++ src/main/java/org/test/bookpub/org/test/bookpub/entity/Author.java
@@ -19,5 +19,53 @@ public class Author {
     @OneToMany(mappedBy = "author")
     private List<Book> books;
 
+    protected Author() {
+
+    }
+
+    public Author(String firstName, String lastName, List<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public Author(String firstName, String lastName) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
 
 }
