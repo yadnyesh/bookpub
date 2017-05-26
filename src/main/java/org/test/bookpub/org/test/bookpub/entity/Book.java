1fa24fee986bb4a30ed5653e57d042f1c1bb4efe
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-26 10:14:08 +0530

    Properties

--- src/main/java/org/test/bookpub/org/test/bookpub/entity/Book.java
+++ src/main/java/org/test/bookpub/org/test/bookpub/entity/Book.java
@@ -90,5 +90,45 @@ public class Book {
                 this.reviewers = reviewers;
         }
 
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
 
+                Book book = (Book) o;
+
+                if (id != null ? !id.equals(book.id) : book.id != null) return false;
+                if (isbn != null ? !isbn.equals(book.isbn) : book.isbn != null) return false;
+                if (title != null ? !title.equals(book.title) : book.title != null) return false;
+                if (description != null ? !description.equals(book.description) : book.description != null)
+                        return false;
+                if (author != null ? !author.equals(book.author) : book.author != null) return false;
+                if (publisher != null ? !publisher.equals(book.publisher) : book.publisher != null) return false;
+                return reviewers != null ? reviewers.equals(book.reviewers) : book.reviewers == null;
+        }
+
+        @Override
+        public int hashCode() {
+                int result = id != null ? id.hashCode() : 0;
+                result = 31 * result + (isbn != null ? isbn.hashCode() : 0);
+                result = 31 * result + (title != null ? title.hashCode() : 0);
+                result = 31 * result + (description != null ? description.hashCode() : 0);
+                result = 31 * result + (author != null ? author.hashCode() : 0);
+                result = 31 * result + (publisher != null ? publisher.hashCode() : 0);
+                result = 31 * result + (reviewers != null ? reviewers.hashCode() : 0);
+                return result;
+        }
+
+        @Override
+        public String toString() {
+                return "Book{" +
+                        "id=" + id +
+                        ", isbn='" + isbn + '\'' +
+                        ", title='" + title + '\'' +
+                        ", description='" + description + '\'' +
+                        ", author=" + author +
+                        ", publisher=" + publisher +
+                        ", reviewers=" + reviewers +
+                        '}';
+        }
 }
