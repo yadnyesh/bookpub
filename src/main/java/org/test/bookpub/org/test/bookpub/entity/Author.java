0fc16c33857bb362bb984427c7c12ab6ccec3232
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-26 10:20:40 +0530

    Yes

--- src/main/java/org/test/bookpub/org/test/bookpub/entity/Author.java
+++ src/main/java/org/test/bookpub/org/test/bookpub/entity/Author.java
@@ -67,5 +67,35 @@ public class Author {
         this.books = books;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Author author = (Author) o;
+
+        if (id != null ? !id.equals(author.id) : author.id != null) return false;
+        if (firstName != null ? !firstName.equals(author.firstName) : author.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(author.lastName) : author.lastName != null) return false;
+        return books != null ? books.equals(author.books) : author.books == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (books != null ? books.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
 }
