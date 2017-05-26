f0d1b7821f8e31e85dd171df268f6c25657b50b2
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-26 11:28:38 +0530

    Yes

--- src/main/java/org/test/bookpub/org/test/bookpub/entity/Publisher.java
+++ src/main/java/org/test/bookpub/org/test/bookpub/entity/Publisher.java
@@ -1,8 +1,14 @@
 package org.test.bookpub.org.test.bookpub.entity;
 
+import java.util.List;
+
 /**
  * Created by z063407 on 5/26/17.
  */
 public class Publisher {
+    private Long id;
 
+    private String name;
+
+    private List<Book> books
 }
