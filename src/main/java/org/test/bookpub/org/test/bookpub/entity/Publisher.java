31ee384d9df6563cc50bf8abb36a9b637da37eaf
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-26 12:46:40 +0530

    Test

--- src/main/java/org/test/bookpub/org/test/bookpub/entity/Publisher.java
+++ src/main/java/org/test/bookpub/org/test/bookpub/entity/Publisher.java
@@ -25,4 +25,27 @@ public class Publisher {
         this.name = name;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }
