5fb97566e9bfce2f32787d8a7e4f3a0bd1ce2e99
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-30 14:23:52 +0530

    Test

--- src/main/java/org/test/bookpub/repository/BookRepository.java
+++ src/main/java/org/test/bookpub/repository/BookRepository.java
@@ -1,8 +1,11 @@
 package org.test.bookpub.repository;
 
+import org.springframework.data.repository.CrudRepository;
+import org.test.bookpub.org.test.bookpub.entity.Book;
+
 /**
  * Created by z063407 on 5/30/17.
  */
-public interface BookRepository {
+public interface BookRepository extends CrudRepository<Book, Long>{
 
 }
