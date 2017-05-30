3d89659477e0f9f47b46f4b618e4c4d1d133f3f3
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-30 14:16:58 +0530

    Test

--- src/main/java/org/test/bookpub/org/test/bookpub/entity/Reviewer.java
+++ src/main/java/org/test/bookpub/org/test/bookpub/entity/Reviewer.java
@@ -1,7 +1,23 @@
 package org.test.bookpub.org.test.bookpub.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by z063407 on 5/26/17.
  */
 public class Reviewer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Reviewer() {}
+
+    public Reviewer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
