b901b9385d8ff13e3b0a2c48f14e0335e8608650
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-31 14:39:20 +0530

    Test

--- src/main/java/org/test/bookpub/StartupRunner.java
+++ src/main/java/org/test/bookpub/StartupRunner.java
@@ -4,6 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.test.bookpub.repository.BookRepository;
 
 import javax.sql.DataSource;
 
@@ -14,8 +15,12 @@ public class StartupRunner implements CommandLineRunner {
     @Autowired
     private DataSource ds;
 
+    @Autowired
+    private BookRepository bookRepository;
+
     @Override
     public void run(String... args) throws Exception {
         logger.info("DataSource: "+ds.toString());
+        logger.info("Number of books: " + bookRepository.count());
     }
 }
