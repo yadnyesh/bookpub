88d004a89036e59334d96557a136b1f064878cf9
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-31 20:27:13 +0530

    Finally Working

--- src/main/java/org/test/bookpub/repository/AuthorRepository.java
+++ src/main/java/org/test/bookpub/repository/AuthorRepository.java
@@ -1,7 +1,9 @@
 package org.test.bookpub.repository;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
+
 /**
  * Created by z063407 on 5/31/17.
  */
-public class AuthorRepository {
+public interface AuthorRepository extends PagingAndSortingRepository{
 }
