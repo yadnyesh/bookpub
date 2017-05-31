7dbd6d5dc3d862ca4a6279e9881f72f3f30203f0
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-31 15:46:35 +0530

    ch02html

--- src/main/java/org/test/bookpub/controllers/BookController.java
+++ src/main/java/org/test/bookpub/controllers/BookController.java
@@ -1,13 +1,24 @@
 package org.test.bookpub.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.test.bookpub.org.test.bookpub.entity.Book;
 import org.test.bookpub.repository.BookRepository;
 
 /**
  * Created by z063407 on 5/31/17.
  */
+
+@RestController
+@RequestMapping("/book")
 public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
+
+
+    public Iterable<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
 }
