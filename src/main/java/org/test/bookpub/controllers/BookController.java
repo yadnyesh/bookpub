85ec1ac49d48804303c4d1bc7cfdd720bb21a4fc
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-31 15:43:00 +0530

    ch02html

--- src/main/java/org/test/bookpub/controllers/BookController.java
+++ src/main/java/org/test/bookpub/controllers/BookController.java
@@ -1,7 +1,13 @@
 package org.test.bookpub.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.test.bookpub.repository.BookRepository;
+
 /**
  * Created by z063407 on 5/31/17.
  */
 public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
 }
