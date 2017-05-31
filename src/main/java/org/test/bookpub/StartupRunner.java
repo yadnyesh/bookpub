d3d5c0b71c143ba18ae5519990533b5adc4d2406
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-31 15:35:53 +0530

    Test

--- src/main/java/org/test/bookpub/StartupRunner.java
+++ src/main/java/org/test/bookpub/StartupRunner.java
@@ -4,6 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.test.bookpub.repository.BookRepository;
 
 import javax.sql.DataSource;
@@ -24,8 +25,9 @@ public class StartupRunner implements CommandLineRunner {
         //logger.info("Number of books: " + bookRepository.count());
     }
 
+    @Scheduled(initialDelay = 1000, fixedDelay = 1000)
     public void run() {
-
+        logger.info("Number of books: " + bookRepository.count());
     }
 
 }
