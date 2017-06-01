1e8455d4ee231ae4847bbd362d4f1f86e39448d8
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-06-01 09:48:45 +0530

    Finally Working

--- src/main/java/org/test/bookpub/WebConfiguration.java
+++ src/main/java/org/test/bookpub/WebConfiguration.java
@@ -3,15 +3,22 @@ package org.test.bookpub;
 import org.apache.catalina.filters.RemoteIpFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 /**
  * Created by z063407 on 6/1/17.
  */
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration extends WebMvcConfigurerAdapter{
 
     @Bean
     public RemoteIpFilter remoteIpFilter() {
         return new RemoteIpFilter();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor();
     }
 }
