6353feb099aa5863ea954861e487fccdf2faf99d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-06-01 06:59:31 +0530

    Finally Working

--- src/main/java/org/test/bookpub/repository/PublisherRepository.java
+++ src/main/java/org/test/bookpub/repository/PublisherRepository.java
@@ -1,7 +1,10 @@
 package org.test.bookpub.repository;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.test.bookpub.org.test.bookpub.entity.Publisher;
+
 /**
  * Created by z063407 on 5/31/17.
  */
-public interface PublisherRepository {
+public interface PublisherRepository extends PagingAndSortingRepository<Publisher, Long> {
 }
--- src/main/java/org/test/bookpub/repository/ReviewerRepository.java
+++ src/main/java/org/test/bookpub/repository/ReviewerRepository.java
@@ -1,10 +1,12 @@
 package org.test.bookpub.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.test.bookpub.org.test.bookpub.entity.Reviewer;
 
 /**
  * Created by z063407 on 5/31/17.
  */
+@RepositoryRestResource
 public interface ReviewerRepository extends PagingAndSortingRepository<Reviewer, Long>{
 }
