7c0eed30f692541f67504d3e080ae6cda40ebdf1
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-06-01 09:21:58 +0530

    Finally Working

---
+++ src/main/java/org/test/bookpub/WebConfiguration.java
@@ -0,0 +1,10 @@
+package org.test.bookpub;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by z063407 on 6/1/17.
+ */
+@Configuration
+public class WebConfiguration {
+}
