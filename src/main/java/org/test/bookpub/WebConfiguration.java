6b3e3d9d650ca153ff69447b6065f9ab217fd30c
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-06-01 10:28:49 +0530

    Finally Working

--- src/main/java/org/test/bookpub/WebConfiguration.java
+++ src/main/java/org/test/bookpub/WebConfiguration.java
@@ -3,6 +3,8 @@ package org.test.bookpub;
 import org.apache.catalina.filters.RemoteIpFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
@@ -20,5 +22,15 @@ public class WebConfiguration extends WebMvcConfigurerAdapter{
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
         return new LocaleChangeInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public ByteArrayHttpMessageConverter byteArrayHttpMessageConverter() {
+        return new ByteArrayHttpMessageConverter();
     }
 }
