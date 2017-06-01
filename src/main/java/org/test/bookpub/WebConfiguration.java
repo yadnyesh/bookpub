1aaffeaad52174baba39578b4d76165ac517dad0
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-06-01 13:36:05 +0530

    Finally Working

--- src/main/java/org/test/bookpub/WebConfiguration.java
+++ src/main/java/org/test/bookpub/WebConfiguration.java
@@ -1,18 +1,34 @@
 package org.test.bookpub;
 
 import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistrar;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.test.bookpub.formatters.BookFormatter;
+import org.test.bookpub.org.test.bookpub.entity.Book;
+import org.test.bookpub.org.test.bookpub.entity.Reviewer;
+import org.test.bookpub.repository.BookRepository;
+
+import java.util.Formatter;
+import java.util.List;
 
 /**
  * Created by z063407 on 6/1/17.
  */
 @Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter{
+
+    @Autowired
+    BookRepository bookRepository;
 
     @Bean
     public RemoteIpFilter remoteIpFilter() {
@@ -32,5 +48,15 @@ public class WebConfiguration extends WebMvcConfigurerAdapter{
     @Bean
     public ByteArrayHttpMessageConverter byteArrayHttpMessageConverter() {
         return new ByteArrayHttpMessageConverter();
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry){
+        registry.addFormatter(new BookFormatter(bookRepository));
+    }
+
+    @RequestMapping(value = "/{isbn}/reviewers", method = RequestMethod.GET)
+    public List<Reviewer> getReviewers(@PathVariable("isbn") Book book) {
+        return book.getReviewers();
     }
 }
