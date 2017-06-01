bea767a7ad926937e4f4b1e26f44363ec25f98de
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-06-01 12:46:24 +0530

    Finally Working

---
+++ src/main/java/org/test/bookpub/Isbn.java
@@ -0,0 +1,18 @@
+package org.test.bookpub;
+
+/**
+ * Created by z063407 on 6/1/17.
+ */
+public class Isbn {
+
+    private String isbn;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+}
